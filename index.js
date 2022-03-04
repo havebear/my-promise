6fd1d5a90cc5f506fba45521d4f90b18cad7e3df
havebear <616749285@qq.com>
2022-03-04 15:48:15 +0800

    chore: 备份代码

--- index.js
+++ index.js
@@ -2,6 +2,44 @@
  * @Author: BGG
  * @Date: 2022-03-04 15:42:30
  * @LastEditors: BGG
- * @LastEditTime: 2022-03-04 15:42:32
+ * @LastEditTime: 2022-03-04 15:47:40
  * @Description:  Promise/A+ 规范实现
  */
+
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected'
+
+class Promise {
+
+  constructor (executor) {
+    executor(this.resolve, this.reject)
+  }
+
+  /** 实例属性 */
+
+  // 状态
+  status = PENDING
+  // 解决值（成功之后的值）
+  value = null
+  // 拒绝原因（失败之后的原因）
+  reason = null
+
+  /** 更改完成后的状态 */
+  resolve = value => {
+    /** 只有等待状态才能修改状态 */
+    if (this.status === PENDING) {
+      this.status = FULFILLED
+      this.value = value
+    }
+  }
+  
+  /** 更改失败后的状态 */
+  reject = reason => {
+    /** 只有等待状态才能修改状态 */
+    if (this.status === PENDING) {
+      this.status = REJECTED
+      this.reason = reason
+    }
+  }
+}
